f0766de141cdc00d59302e118dd8c7521efaca2b
christabor <dxdstudio@gmail.com>
2014-03-16 18:56:02 -0700

    Remove semantic indices and clean up examples dictionary

--- web/namebot/examples.py
+++ web/namebot/examples.py
@@ -35,24 +35,24 @@ def example_metrics(filename=None):
     allnames = test.open_file(filename)
     results = {
         'names': allnames,
-        'metrics': {}
+        'metrics': {
+            'digits_freq': test.get_digits_frequency(allnames),
+            'length': test.name_length(allnames),
+            'vowel_beginning': test.name_starts_with_vowel(allnames),
+            'vowel_count': test.name_vowel_count(allnames),
+            'name_length': test.name_length(allnames),
+            'name_spaces': test.get_name_spaces(allnames),
+            'consonant_repeat_freq': test.get_consonant_repeat_frequency(allnames),
+            'consonant_duplicate_repeat_freq': test.get_consonant_duplicate_repeat_frequency(allnames),
+            'vowel_repeat_freq': test.get_vowel_repeat_frequency(allnames),
+            'special_characters': test.get_special_chars(allnames),
+            'search_results': test.get_search_results(allnames),
+            'name_numbers': test.get_named_numbers(allnames),
+            'adj_verb_noun': test.get_adjective_verb_or_noun(allnames),
+            'first_letter_freq': test.get_first_letter_frequency(allnames),
+            'word_types': test.get_word_types(allnames)
+        }
     }
-
-    results['metrics']['digits_frequency'] = test.get_digits_frequency(allnames)
-    results['metrics']['length'] = test.name_length(allnames)
-    results['metrics']['vowel_beginning'] = test.name_starts_with_vowel(allnames)
-    results['metrics']['vowel_count'] = test.name_vowel_count(allnames)
-    results['metrics']['name_length'] = test.name_length(allnames)
-    results['metrics']['get_name_spaces'] = test.get_name_spaces(allnames)
-    results['metrics']['get_consonant_repeat_frequency'] = test.get_consonant_repeat_frequency(allnames)
-    results['metrics']['consonant_duplicate_repeat_frequency'] = test.get_consonant_duplicate_repeat_frequency(allnames)
-    results['metrics']['vowel_repeat_frequency'] = test.get_vowel_repeat_frequency(allnames)
-    results['metrics']['special_characters'] = test.get_special_chars(allnames)
-    results['metrics']['search_results'] = test.get_search_results(allnames)
-    results['metrics']['name_numbers'] = test.get_named_numbers(allnames)
-    results['metrics']['adj_verb_noun'] = test.get_adjective_verb_or_noun(allnames)
-    results['metrics']['get_first_letter_frequency'] = test.get_first_letter_frequency(allnames)
-    results['metrics']['get_word_types'] = test.get_word_types(allnames)
     return results
 
 
@@ -61,9 +61,10 @@ def generate_all_examples(filename=None, words=None):
     print '... -=|=- Running ALL the examples -=|=- ... '
     print '< ##=========##===========##=========## > '
 
-    example_data = {}
-    example_data['synsets'] = example_synsets(words=words)
-    example_data['metrics'] = example_metrics(filename=filename)
-    example_data['techniques'] = example_techniques(words=words)
-    example_data['scoring'] = example_scoring(words=words)
+    example_data = {
+        'synsets': example_synsets(words=words),
+        'metrics': example_metrics(filename=filename),
+        'techniques': example_techniques(words=words),
+        'scoring': example_scoring(words=words)
+    }
     return example_data
