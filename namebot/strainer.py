1cff91335037c0fd2cf96695849e37bf2b7bc5e6
christaborium <dxdstudio@gmail.com>
2016-03-07 20:46:49 -0800

    Add missing docstrings and fill out remaining filter methods.

--- namebot/strainer.py
+++ namebot/strainer.py
@@ -1,8 +1,6 @@
-import re
-import scoring
-import settings as namebot_settings
+"""Provides filtering functions for reducing large sets of generated words.
 
-"""Recommended usage: filtered results using a list comprehension
+Recommended usage: filtered results using a list comprehension
 of all filters 'anded' together.
 e.g:
 filtered = [word for word in words if
@@ -12,10 +10,16 @@ filtered = [word for word in words if
             filter_endswith(word, ending='e') ...]
 """
 
+import re
+import scoring
+import settings as namebot_settings
+
 
 def filter_vowel_cons_ratio(word, ratio=0.5):
     """Return True if the ratio of vowels to consonants is > `ratio`.
-    This can be used as an ad-hoc pronunciation filter."""
+
+    This can be used as an ad-hoc pronunciation filter.
+    """
     vowels = re.compile(r'[aeiouy]')
     consonants = re.compile(r'[^aeyiuo]')
     vmatch = re.findall(vowels, word.lower())
@@ -27,37 +31,100 @@ def filter_vowel_cons_ratio(word, ratio=0.5):
 def filter_length(word,
                   min_length=namebot_settings.MIN_LENGTH,
                   max_length=namebot_settings.MAX_LENGTH):
+    """Filter based on min and max lengths.
+
+    Args:
+        word (str): The word.
+        min_length (int, optional): The minimum allowed length.
+        max_length (int, optional): The maximum allowed length.
+
+    Returns:
+        bool: The resulting check.
+    """
     return len(word) >= min_length and len(word) <= max_length
 
 
 def filter_startswith(word, beginning=None):
+    """Filter a word starting with specified string.
+
+    Args:
+        word (str): The word.
+        beginning (str, optional): The beginning string to check.
+
+    Returns:
+        bool: The resulting check.
+    """
     return word.lower().startswith(beginning)
 
 
 def filter_endswith(word, ending=None):
+    """Filter words ending with a specified suffix.
+
+    Args:
+        word (str): A word.
+        ending (str, optional): The optional ending to check.
+
+    Returns:
+        bool: The resulting check.
+    """
     return word.lower().endswith(ending)
 
 
 def filter_tld(word, tld='.com'):
+    """Check if a word ends with a TLD suffix (can be used to make a valid TLD).
+
+    Args:
+        word (str): The wor.d
+        tld (str, optional): The TLD to check against, with or without a dot.
+
+    Returns:
+        bool: The resulting check.
+    """
     if tld.startswith('.'):
         tld = tld.replace('.', '')
     return filter_endswith(word, ending=tld)
 
 
 def filter_metaphone(word):
-    """Get the metaphone value, and if the second key is None
-    pronunciation is unlikely."""
+    """Get metaphone value, checking score.
+
+    Where second key being None, an easy pronunciation is unlikely.
+
+    Args:
+        word (str): The word.
+
+    Returns:
+        bool: The resulting check.
+    """
     score = scoring.score_dmetaphone(word)
     return score[1] is not None
 
 
-def filter_soundex(word):
-    # TODO
-    # res = nameScoring.score_soundex(word)
-    pass
+def filter_soundex(word, code=None):
+    """Get soundex value, checking result against a given code.
+
+    Args:
+        word (str): The word.
+        code (str, optional): The soundex pronunciation code.
+
+    Returns:
+        bool: The resulting check.
+    """
+    if code is None:
+        return True
+    return scoring.score_soundex(word).split(':')[1].strip() == code
 
 
-def filter_nysiis(word):
-    # TODO
-    # res = nameScoring.score_nysiis(word)
-    pass
+def filter_nysiis(word, code=None):
+    """Get nysiis value, checking score.
+
+    Args:
+        word (str): The word.
+        code (str, optional): The nysiis pronunciation code.
+
+    Returns:
+        bool: The resulting check.
+    """
+    if code is None:
+        return True
+    return scoring.score_nysiis(word).split(':')[1].strip() == code
